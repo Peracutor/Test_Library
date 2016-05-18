e662af3364beb147ba966feea14904b20b8dd727
Peracutor <arkanseidos@gmail.com>
2016-05-18 14:17:23 +0200

    Initial commit

---
+++ testlibrary/src/main/java/com/eissler/micha/testlibrary/TestLibApi.java
@@ -0,0 +1,12 @@
+package com.eissler.micha.testlibrary;
+
+/**
+ * Created by Micha.
+ * 18.05.2016
+ */
+public class TestLibApi {
+
+    public String getTestString() {
+        return "If you get this it works";
+    }
+}
